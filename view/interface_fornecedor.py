5a94d6404aef73614be7eae0a46e29cfc9f07aa4
AlmadaLuiz14 <105432238+AlmadaLuiz14@users.noreply.github.com>
2022-12-08 18:48:47 -0300

    Criação da scrollbar

--- view/interface_fornecedor.py
+++ view/interface_fornecedor.py
@@ -12,8 +12,20 @@ class interface_fornecedor(interface):
 
     def __init__(self, nome, raiz, vf):
         super().__init__(nome, raiz)
+        
+        #Criei um frame e uma scrollbar nesse frmae
+        self.frame = Frame(raiz)
+        self.sb = Scrollbar(self.frame, orient=VERTICAL)
+        
         self.vf = vf
-        self.table = table(raiz, vf)
+        self.table = table(raiz, vf, self.frame, self.sb)
+        self.table.table.pack(side=LEFT)
+        
+        #configurei a scrollbar
+        self.sb.config(command=self.table.table.yview)
+        self.sb.pack(side=RIGHT, fill=Y)
+        self.frame.pack()
+        
         self.bt1 = Button(raiz, text='Cadastrar Fornecedor', command=self.JanelaAdd)
         self.bt1.pack(side=LEFT)
         self.bt2 = Button(raiz, text='Fechar', command=self.fechar)
